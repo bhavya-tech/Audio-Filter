21d3613440f9d3044000233c43c97aee10fd2be4
Bhavya Peshavaria <64970880+bhavya-tech@users.noreply.github.com>
2020-10-18 22:55:32 +0530

    updated gui

--- runner.py
+++ runner.py
@@ -10,11 +10,15 @@ from matplotlib.backends.backend_wxagg import \
         NavigationToolbar2WxAgg as NavigationToolbar
 
 class Runner(noname.MyFrame1):
+    
     def __init__(self,parent):
         noname.MyFrame1.__init__(self,parent)
 
     def render(self, event):
-        self.figure = Figure()#figsize=(6, 4), dpi=100)
+        dpi = getDpi()
+        w, h = self.m_panel2.GetSize()
+        
+        self.figure = Figure(figsize=(w/dpi,h/dpi))
         self.axes = self.figure.add_subplot(111)
         t,s = engine.get_input_plot()
         self.axes.plot(t,s)
@@ -22,6 +26,11 @@ class Runner(noname.MyFrame1):
         self.canvas = FigCanvas(self.m_panel2, wx.ID_ANY, self.figure) 
 
         return
+
+def getDpi():
+    px = float(wx.GetDisplaySize()[1])
+    mm = float(wx.GetDisplaySizeMM()[1])
+    return (px/(mm*0.0393701))
 
 def main():
     app = wx.App()
